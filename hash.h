eef2f67d92f2fdbc167d793c29b301ad00cf3384
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-10-18 01:11:24 +0000

    Writting hash header

--- hash.h
+++ hash.h
@@ -1,10 +1,18 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include"list.h"
+
 class Hash
 {
+	private:
+		int tam;
+		List vetor**;
 	public:
-	protected:
+		int hash_function(/* key */);	//(0 =< x < tam)
+		bool insert(Item*);
+		Item* remove(Item*);
+		Item* select(Item*);
 };
 
 #endif
