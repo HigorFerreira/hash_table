7fa28cd555064940d4589c3a6dc40be521768446
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-10-22 00:09:49 +0000

    Starting to build menu

--- main.cpp
+++ main.cpp
@@ -4,12 +4,32 @@
 using namespace std;
 
 int main(){
-	/*
-	Hash *h = new Hash(10);
-	Item *i = new Item;
+	bool op = true;
+	Hash *th = new Hash(10);
 	
-	i->setNome("Higor Ferreira Alves Santos");
-	i->setEmail("hfashigor2@gmail.com");
-	*/
+	Item *pessoa;
+	
+	string aux;
+	char auxc;
+	double auxd;
+	
+	do{
+		pessoa = new Item;
+		cout<<"================ Hash Table ================\n";
+		cout<<"Digite o nome: ";
+		cin>>aux;
+		pessoa->setNome(aux);
+		cout<<"Digite o email: ";
+		cin>>aux;
+		pessoa->setEmail(aux);
+		cout<<"Digite o sexo: ";
+		cin>>auxc;
+		pessoa->setSexo(auxc);
+		cout<<"Digite o Telefone: ";
+		cin>>auxd;
+		pessoa->setTelefone(auxd);
+		cout<<"-------------------------------------------\n";
+	}while(op == true);
+	
 	
 }
